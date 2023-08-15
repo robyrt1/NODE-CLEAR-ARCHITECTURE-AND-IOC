01d51316749b08255b51c16730ff6c994273b876
Robert P. Mendes <robertpinto1190.rpm@gmail.com>
2023-08-15 17:35:29 -0400

    add response de error

--- src/infratructure/@shared/httpResponse/httpResponseMappingHandler.ts
+++ src/infratructure/@shared/httpResponse/httpResponseMappingHandler.ts
@@ -1,11 +1,19 @@
-import { IHttpResponseMappingHandler } from "../interfaces/httpResponseMappingHandlerInterface";
+import { get } from 'lodash';
+import { IHttpResponseMappingHandler } from '../interfaces/httpResponseMappingHandlerInterface';
+import { HTTPCODE } from '../constants/httpCode';
 
 export const httpResponseMappingHandler = <T>(
-    status: boolean,
-    message: string,
-    data: T,
-    statusCode?: number,
-    success?:string
+  status: boolean,
+  message: string,
+  data: T,
+  statusCode?: number,
+  success?: string,
 ): IHttpResponseMappingHandler<T> => {
-    return { status, message, statusCode,success,data};
+  return { status, message, statusCode, success, data };
+};
+
+export const httpResponseMappingHandlerError = (error: Error): IHttpResponseMappingHandler<[]> => {
+  const messageFail = get(error, 'message', error) as any;
+  const statusCode = get(error, 'statusCode', HTTPCODE.INTERNAL_SERVER_ERROR);
+  return httpResponseMappingHandler(false, messageFail, [], statusCode, 'fail');
 };
