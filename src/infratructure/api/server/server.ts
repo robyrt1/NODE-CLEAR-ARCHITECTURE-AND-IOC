b9b8e6211f17d6935b270b1421a748dc7d8a6789
robyrt1 <robertpinto1190.rpm@gmail.com>
2024-05-06 11:16:34 -0400

    info router console

--- src/infratructure/api/server/server.ts
+++ src/infratructure/api/server/server.ts
@@ -2,9 +2,10 @@ import "reflect-metadata";
 import express  from "express";
 import morgan from 'morgan'
 import cors from 'cors';
-import { InversifyExpressServer} from "inversify-express-utils";
+import { InversifyExpressServer, getRouteInfo} from "inversify-express-utils";
 import container from "../conf/inversify/inversify.config";
 import '../conf/routes/routes'
+import { map } from "lodash";
 
 const app = new InversifyExpressServer(container);
 
@@ -25,7 +26,14 @@ app.setConfig((app) => {
 
 const server = app.build();
 const portServer = Number(process.env.PORT);
+const routeInfo = getRouteInfo(container)
 
+map(routeInfo, enpoints => {
+  const nameController = enpoints.controller;
+  map(enpoints.endpoints, endpoint => {
+    console.log(`\x1b[34mLOG [RouterExplorer] - ${nameController} - {${endpoint.route}}\x1b[0m`);
+  });
+});
 const serverRun = server.listen(portServer,()=>{
   const address: any= serverRun.address();
   console.log(`[INFO] - Server connection: http://${address.address}:${address.port}`);
